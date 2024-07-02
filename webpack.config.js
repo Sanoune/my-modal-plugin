b0a7e7be5650e9fcf187b250199cb12c8e828d7b
Aba <sarah.aba@hotmail.fr>
2024-07-02 15:38:12 +0200

    modal Plugin

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve("dist"),
+    filename: "index.js",
+    library: "myModalPlugin",
+    libraryTarget: "umd",
+    globalObject: "this",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  externals: {
+    react: "react",
+    "react-dom": "react-dom",
+  },
+};
